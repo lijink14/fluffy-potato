4659e71a048fbd059929867b04e6b14f6fd8b7d3
Lijin K <lijink14@gmail.com>
2019-09-20 17:29:20 +0530

    Finished code for HTTP module

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 var http = require('http');
-
+var fs = require('fs');
+var path = require('path');
 var hostname = 'localhost';
 var port = 3000;
-
-var server = http.createServer((req,resp)=>{
-
-    console.log("the request recieved headers"+"   "+JSON.stringify(req.headers));
-
-    resp.statusCode=200;
-    resp.statusMessage= "sucess";
-    resp.setHeader('Content-Type','text/html')
-    resp.end('<html><h1>Hello</h1></html>') 
+var server = http.createServer((req, resp) => {
+    if (req.method == "GET") {
+        var fileUrl;
+        if (req.url == "/") { fileUrl = "/index.html"; }
+        else { fileUrl = req.url; }
+        var filepath = path.resolve('./' + fileUrl);
+        var fileex = path.extname(filepath);
+        if (fileex == ".html") {
+            fs.exists(filepath, (exists) => {
+                if (!exists) {
+                    resp.statusMessage = "Error:file not found"
+                    resp.statusCode = 404;
+                    resp.setHeader('Content-Type', 'text/html');
+                    resp.end('<html><h1>the file not found error</h1></html>');
+                    return;
+                }
+                else {
+                    resp.setHeader('Content-Type', 'text/html');
+                    resp.statusCode = 200;
+                    fs.createReadStream(filepath).pipe(resp);
+                    resp.statusMessage = "Sucess";
+                }
+            })
+        }
+        else {
+            resp.statusCode = 500;
+            resp.statusMessage = "the file is not an html";
+            resp.setHeader('Content-Type', 'text/text');
+            resp.end('<html><h1>the file is not an html</h1></html>');
+            return;
+        }
+    }
+    else {
+        resp.statusCode = 500;
+        resp.statusMessage = "failed";
+        resp.setHeader('Content-Type', 'text/html')
+        resp.end('<html><h1>the method was not get</h1></html>')
+        return;
+    }
 })
-
-server.listen(port,hostname,()=>{
-
+server.listen(port, hostname, () => {
     console.log(`The server is listining to the port http://${port}:${hostname}`);
 })
